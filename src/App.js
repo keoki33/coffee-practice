756bd201ce01c3640e9df763d8afd32ce258b92b
George <gwong323@yahoo.com>
2019-11-03 22:32:40 +0000

    start setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import About from "./About";
+// import Navbar from "./Navbar";
+// import Contact from "./Contact";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.scss";
+
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+class App extends Component {
+  state = {};
+
+  whatever = () => {};
+
+  render() {
+    return (
+      <Router>
+        <div className="main">
+          {/* <Navbar /> */}
+          <Switch>
+            <Route exact path={["/", "/home"]} render={props => <Home />} />
+            {/* <Route path="/about" render={props => <About />} />
+            <Route path="/contact" render={props => <Contact />} /> */}
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+// import Footer from "./Footer";
+
+// import increment from "./actions";
+
+class Home extends Component {
+  state = {};
+  // const counterReducer = useSelector(state => state.counterReducer);
+  whatever = () => {};
+
+  test = () => {};
+
+  render() {
+    return (
+      <div className="home">
+        <h1>Home {this.test()}</h1>
+        <button>+</button>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          deleniti praesentium fugiat, neque ea ullam modi quaerat quam officia
+          in obcaecati voluptatibus quis autem nisi repudiandae perspiciatis
+          earum recusandae veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          deleniti praesentium fugiat, neque ea ullam modi quaerat quam officia
+          in obcaecati voluptatibus quis autem nisi repudiandae perspiciatis
+          earum recusandae veritatis.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          deleniti praesentium fugiat, neque ea ullam modi quaerat quam officia
+          in obcaecati voluptatibus quis autem nisi repudiandae perspiciatis
+          earum recusandae veritatis.
+        </p>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+}
+
+export default Home;
